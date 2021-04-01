8b1b0e11e551a48ff5c42624d0e8940b45f474c5
Даниил <highfive111aaa@gmail.com>
2021-04-01 10:32:45 +0300

    deleted comments

--- sna/sna/urls.py
+++ sna/sna/urls.py
@@ -6,7 +6,6 @@ from registration.views import RegistrationView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='/'),
-    # path('', TemplateView.as_view(template_name='methods/description.html'), name='/methods'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', RegistrationView.as_view(), name='registration')
