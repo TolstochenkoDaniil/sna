6eac8c85f27f10c488653f60531eeec130c5f0c9
Daniil Tolstochenko <pochtobox@mail.ru>
2021-05-16 13:25:40 +0300

    Added new mail massage

--- sna/registration/signals.py
+++ sna/registration/signals.py
@@ -7,10 +7,26 @@ from django.core.mail import EmailMessage, get_connection
 
 MESSAGE_TITLE= 'Welcome to snar'
 MESSAGE_BODY = '''
-    Hello, {name}!
+Dear {name},
 
-    We\'re glad you\'ve decided to participate in our research.
-    '''
+Thank you for registering for this experiment!
+
+The purpose of this experiment is to identify the most effective method of preventing excessive
+social media use. Three methods will be tested in this experiment:
+1. Time restriction
+2. Notification blocker
+3. Delete apps.
+
+The duration of the experiment is 30 days during which all the participants are required to
+record their social media usage.
+A member of the research group will be in touch with you shortly to allocate you to one of
+below methods.
+
+Login at http://snar-309908.lm.r.appspot.com/
+
+Best regards,
+SNAR team
+'''
 
 
 @receiver(signal=post_save, sender=User)
