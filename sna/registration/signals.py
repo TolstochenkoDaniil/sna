e4da4ab767dd47cfb965c4212a4d09171ca2eef8
Daniil Tolstochenko <pochtobox@mail.ru>
2021-05-16 23:33:31 +0300

    Adde methods details

--- sna/registration/signals.py
+++ sna/registration/signals.py
@@ -18,13 +18,25 @@ social media use. Three methods will be tested in this experiment:
 3. Delete apps.
 
 The duration of the experiment is 30 days during which all the participants are required to
-record their social media usage.
-A member of the research group will be in touch with you shortly to allocate you to one of
-below methods.
+record their social media usage daily.
+
+An email from research manager will allocate you to one of methods which you will be using for
+the duration of these thirty days.
+Facebook, YouTube, and Instagram are the only social media apps which are used for the
+study.
+
+If you continue the excessive use of social, continue recording the data.
+
+You will be contacted for an interview after the completion of the experiment.
 
 Login at http://snar-309908.lm.r.appspot.com/
 
-Best regards,
+Thank you and good luck!
+
+REMINDER: if you fail to follow the instructions for thirty days and end up using social media
+as you normally do, please continue recording the amount of hours you do so until the
+experiment has reached its thirty day limit.
+
 SNAR team
 '''
 
@@ -36,7 +48,7 @@ def send_welcome_mail(sender: User, instance: User, created: bool, **kwargs) -> 
         with get_connection() as conn:
             EmailMessage(
                 MESSAGE_TITLE,
-                MESSAGE_BODY.format(name=instance.first_name),
+                MESSAGE_BODY.format(name=instance.username),
                 settings.EMAIL_HOST_USER,
                 (instance.email,),
                 connection=conn
